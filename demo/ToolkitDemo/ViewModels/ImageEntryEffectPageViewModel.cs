c8097347253dfa7d74e9a7868242540209f61661
Zohaib Hanif <zohaib@avantipoint.com>
2019-10-23 13:09:22 +0500

    Added ImageEntryEffect demo
    - Added page for ImageEntryEffect with respective view model.
    - Updated MenuGroup to add Effects group.
    - Added assembly attribute in ImageEntryEffectPage.xaml.cs.
    fixes #88

---
+++ demo/ToolkitDemo/ViewModels/ImageEntryEffectPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Prism.Logging;
+using Prism.Navigation;
+using Prism.Services;
+
+namespace ToolkitDemo.ViewModels
+{
+    public class ImageEntryEffectPageViewModel : DemoPageViewModelBase
+    {
+        public ImageEntryEffectPageViewModel(INavigationService navigationService, IPageDialogService pageDialogService, ILogger logger)
+            : base(navigationService, pageDialogService, logger)
+        {
+        }
+    }
+}
---
+++ demo/ToolkitDemo/Views/ImageEntryEffectPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ToolkitDemo.Helpers;
+using ToolkitDemo.Views;
+using Xamarin.Forms;
+
+[assembly: MenuItem("ImageEntry Effect", nameof(ImageEntryEffectPage), MenuGroup.Effects)]
+namespace ToolkitDemo.Views
+{
+    public partial class ImageEntryEffectPage : ContentPage
+    {
+        public ImageEntryEffectPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
